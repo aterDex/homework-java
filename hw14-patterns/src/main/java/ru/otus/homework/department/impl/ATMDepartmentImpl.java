77bccec78b618a47e894a5c2a4d62b7af975cfd5
eugene <eugene.s@nexttehnika.ru>
2020-08-25 15:48:41 +1000

    there added restore to ATMDepartmentImpl

--- hw14-patterns/src/main/java/ru/otus/homework/department/impl/ATMDepartmentImpl.java
+++ hw14-patterns/src/main/java/ru/otus/homework/department/impl/ATMDepartmentImpl.java
@@ -1,4 +1,24 @@
 package ru.otus.homework.department.impl;
 
+import ru.otus.homework.atm.ATM;
+import ru.otus.homework.atm.Snapshot;
+import ru.otus.homework.atm.impl.SnapshotDeMultiplexer;
+
+import java.util.List;
+
 public class ATMDepartmentImpl {
+
+    private final List<? extends ATM> atms;
+    private final Snapshot firstSnapshot;
+
+    public ATMDepartmentImpl(List<? extends ATM> atms) {
+        if (atms == null)
+            throw new IllegalArgumentException("Atms mustn't be null.");
+        this.atms = atms;
+        this.firstSnapshot = SnapshotDeMultiplexer.createFromMementos(atms);
+    }
+
+    public void restoreAtms() {
+        this.firstSnapshot.restore();
+    }
 }
