5936711bfe2a82ab253ef6d74f6cd6f0deb903ec
eugene <eugene.s@nexttehnika.ru>
2020-09-21 15:02:57 +1000

    work with hibernate

--- hw21-hibernate/src/main/java/ru/otus/homework/hw21/HomeWork.java
+++ hw21-hibernate/src/main/java/ru/otus/homework/hw21/HomeWork.java
@@ -1,4 +1,37 @@
 package ru.otus.homework.hw21;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import ru.otus.homework.hw21.core.model.AddressDataSet;
+import ru.otus.homework.hw21.core.model.PhoneDataSet;
+import ru.otus.homework.hw21.core.model.User;
+import ru.otus.homework.hw21.core.service.DbServiceUserImpl;
+import ru.otus.homework.hw21.hibernate.HibernateUtils;
+import ru.otus.homework.hw21.hibernate.dao.UserDaoHibernate;
+import ru.otus.homework.hw21.hibernate.sessionmanager.SessionManagerHibernate;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class HomeWork {
+
+    public static final String HIBERNATE_CFG_FILE = "hibernate.cfg.xml";
+    private static final Logger logger = LoggerFactory.getLogger(HomeWork.class);
+
+    public static void main(String[] args) {
+        var sessionFactory = HibernateUtils.buildSessionFactory(HIBERNATE_CFG_FILE, User.class, AddressDataSet.class, PhoneDataSet.class);
+        try (var sessionManager = new SessionManagerHibernate(sessionFactory)) {
+            var dbServiceUser = new DbServiceUserImpl(new UserDaoHibernate(sessionManager));
+            var id = dbServiceUser.saveUser(new User(0, "dbServiceUser",
+                    new AddressDataSet(0, "address"),
+                    new ArrayList<>(List.of(
+                            new PhoneDataSet(0, "8439543532"),
+                            new PhoneDataSet(0, "3782432874872")
+                    ))));
+            dbServiceUser.getUser(id).ifPresentOrElse(
+                    crUser -> logger.info("user created, {}", crUser.toString()),
+                    () -> logger.info("user didn't create")
+            );
+        }
+    }
 }
--- hw21-hibernate/src/main/java/ru/otus/homework/hw21/core/model/User.java
+++ hw21-hibernate/src/main/java/ru/otus/homework/hw21/core/model/User.java
@@ -2,6 +2,7 @@ package ru.otus.homework.hw21.core.model;
 
 
 import javax.persistence.*;
+import java.util.List;
 
 @Entity
 @Table(name = "users")
@@ -15,12 +16,21 @@ public class User {
     @Column(name = "name")
     private String name;
 
+    @OneToOne(cascade = CascadeType.ALL)
+    private AddressDataSet addressDataSet;
+
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "user", orphanRemoval = true)
+    private List<PhoneDataSet> phoneDataSet;
+
     public User() {
     }
 
-    public User(long id, String name) {
+    public User(long id, String name, AddressDataSet addressDataSet, List<PhoneDataSet> phoneDataSet) {
         this.id = id;
         this.name = name;
+        this.addressDataSet = addressDataSet;
+        this.phoneDataSet = phoneDataSet;
+        updateParent(this.phoneDataSet);
     }
 
     public long getId() {
@@ -39,11 +49,34 @@ public class User {
         this.name = name;
     }
 
+    public AddressDataSet getAddressDataSet() {
+        return addressDataSet;
+    }
+
+    public void setAddressDataSet(AddressDataSet addressDataSet) {
+        this.addressDataSet = addressDataSet;
+    }
+
+    public List<PhoneDataSet> getPhoneDataSet() {
+        return phoneDataSet;
+    }
+
+    public void setPhoneDataSet(List<PhoneDataSet> phoneDataSet) {
+        this.phoneDataSet = phoneDataSet;
+        updateParent(this.phoneDataSet);
+    }
+
+    protected void updateParent(List<PhoneDataSet> phoneDataSet) {
+        phoneDataSet.forEach(x -> x.setUser(this));
+    }
+
     @Override
     public String toString() {
         return "User{" +
                 "id=" + id +
                 ", name='" + name + '\'' +
+                ", addressDataSet='" + addressDataSet + '\'' +
+                ", phoneDataSet='" + phoneDataSet + '\'' +
                 '}';
     }
 }
