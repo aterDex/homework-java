53630a6460f66ddfcf5e8541c4ee21269949530d
eugene <eugene.s@nexttehnika.ru>
2020-10-01 11:05:53 +1000

    create static constant

--- hw24-web-server/src/main/java/ru/otus/homework/web/services/LoginServiceByDBServiceUser.java
+++ hw24-web-server/src/main/java/ru/otus/homework/web/services/LoginServiceByDBServiceUser.java
@@ -2,16 +2,14 @@ package ru.otus.homework.web.services;
 
 import org.eclipse.jetty.security.AbstractLoginService;
 import org.eclipse.jetty.util.security.Password;
-import ru.otus.homework.data.core.model.User;
 import ru.otus.homework.data.core.service.DBServiceUser;
-
-import java.util.Optional;
 
 public class LoginServiceByDBServiceUser extends AbstractLoginService {
 
+    private static final String[] ADMIN_ROLE = {"admin", "user"};
+    private static final String[] USERS_ROLE = {"user"};
+
     private final DBServiceUser userServices;
-    private final String[] ADMIN_ROLE = {"admin", "user"};
-    private final String[] USERS_ROLE = {"user"};
 
     public LoginServiceByDBServiceUser(DBServiceUser userServices) {
         this.userServices = userServices;
@@ -19,16 +17,13 @@ public class LoginServiceByDBServiceUser extends AbstractLoginService {
 
     @Override
     protected String[] loadRoleInfo(UserPrincipal userPrincipal) {
-        if (userPrincipal.getName().equals("admin")) {
-            return ADMIN_ROLE;
-        } else {
-            return USERS_ROLE;
-        }
+        return "admin".equals(userPrincipal.getName()) ? ADMIN_ROLE : USERS_ROLE;
     }
 
     @Override
     protected UserPrincipal loadUserInfo(String login) {
-        Optional<User> dbUser = userServices.findByLogin(login);
-        return dbUser.map(u -> new UserPrincipal(u.getLogin(), new Password(u.getPassword()))).orElse(null);
+        return userServices.findByLogin(login)
+                .map(u -> new UserPrincipal(u.getLogin(), new Password(u.getPassword())))
+                .orElse(null);
     }
 }
